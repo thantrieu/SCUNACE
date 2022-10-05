06cb0ead4412177aaa5686236971d8893115e367
Trieu Than <trieuthan92pro@gmail.com>
2022-10-05 10:20:06 +0700

    Add some update

--- Exercises13.c
+++ Exercises13.c
@@ -10,12 +10,20 @@ int main() {
     int start;
     printf("%s", "Giá trị bắt đầu 1 <= n <= 30: ");
     scanf("%d", &start);
+    if (start <= 0 || start > 30) {
+        puts("Dỗi. Đi mà nhập lại nhé. Ahihi.");
+        return; // kết thúc chương trình luôn
+    }
     printf("%s", "Chiều cao: ");
     int width;
     scanf("%d", &width);
     printf("%s", "Chiều rộng: ");
     int height;
     scanf("%d", &height);
+    if (width < 1 || height < 1) { // không có hình khối nào có độ dài các cạnh <= 0. Nhập 1 không có ý nghĩa.
+        puts("Dỗi luôn. Không có kết quả đâu cưng!");
+        return;
+    }
     int counter1 = start;
     int counter2 = start + width;
     int counter3 = start + 2 * width + height - 3;
